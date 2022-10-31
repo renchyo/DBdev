0cf07eefdcc87c9d68b8c18dd7f24529b5a8426c
renchyo <112875546+renchyo@users.noreply.github.com>
2022-10-31 12:53:21 +0300

    Update Lesson_11(Doronin).sql

--- Lesson_11(Doronin).sql
+++ Lesson_11(Doronin).sql
@@ -1,10 +1,10 @@
 Доронин 903гр.
-4.1
+6.1
 UNION = {L,M,N,O,P,Q,R,S,T}
 UNION ALL = {L,M,N,O,P,P,Q,R,S,T}
 INTERSECT = { P }
 EXCEPT = {L,M,N,O}
-4.2
+6.2
 SELECT last_name, first_name from actor where last_name like 'L%' union all SELECT last_name, first_name from customer where last_name like 'L%';
-4.3
+6.3
 SELECT last_name, first_name from actor where last_name like 'L%' union all SELECT last_name, first_name from customer where last_name like 'L%' order by last_name;
