fc6a275af8a9695215e83f3a8ad4ac4155d9fcac
renchyo <112875546+renchyo@users.noreply.github.com>
2022-11-12 10:04:31 +0300

    Lesson_9(Доронин 903гр)

---
+++ lesson_9.sql
@@ -0,0 +1,12 @@
+select sakila;
+4.3.
+select * from payment where amount in ('1.98', '7.98', '9.98');
+4.4.
+select * from customer where last_name like '_A%W%';
+
+К проекту:
+
+1. 
+select * from patient where age in ('30', '32', '40');
+2.
+select * from doctors where last_name like '_B%A%';
